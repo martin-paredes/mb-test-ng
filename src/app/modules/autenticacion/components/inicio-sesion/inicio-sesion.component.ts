2f69d3e0af7e438fd2dcbf81ec7f7108adb27af9
Martín Paredes <martin.paredes@lucasianmexico.com>
2024-06-19 08:04:25 -0600

    Ajuste inicial para formulario de login y redireccionamiento a pantalla de registro

--- src/app/modules/autenticacion/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/modules/autenticacion/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.sass'
 })
-export class InicioSesionComponent {
+export class InicioSesionComponent implements OnInit {
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+  irARegistrarse(): void {
+    void this.router.navigate(['/registro']);
+  }
 }
