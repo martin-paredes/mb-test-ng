6ce865bc759cafef682fd1008dba6e736708f107
Martín Paredes <martin.paredes@lucasianmexico.com>
2024-06-20 21:09:30 -0600

    Maquetación componente header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,16 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
 export class HeaderComponent {
+  public numNotificaciones: number = 0;
+  public numArtCarritoCompras: number = 5;
 
+  constructor() { }
 }
