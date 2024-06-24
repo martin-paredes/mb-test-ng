4262d8f1033d2ca8bbdba7aaa6d17e4f8de33367
Martín Paredes <martin.paredes@lucasianmexico.com>
2024-06-23 18:02:05 -0600

    Se realiza maqueta para componente de Oferta

--- src/app/modules/pagina-principal/components/ofertas/ofertas.component.ts
+++ src/app/modules/pagina-principal/components/ofertas/ofertas.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Oferta } from '../../models/oferta';
 
 @Component({
   selector: 'app-ofertas',
@@ -8,5 +9,49 @@ import { Component } from '@angular/core';
   styleUrl: './ofertas.component.sass'
 })
 export class OfertasComponent {
+  @Input()
+  ofertas: Oferta[] = [
+    {
+      sku: 'DCATO19939',
+      srcImg: 'assets/imagenes/pagina-principal/ofertas/radiador.svg',
+      descripcionProducto: 'Radiador agricola tractor Case 580 k Aluminio/Aluminio TM',
+      precio: '$ 1,842',
+      oferta: '$ 1,842',
+      ranking: '4.9',
+    },
+    {
+      sku: 'DCATO19939',
+      srcImg: 'assets/imagenes/pagina-principal/ofertas/aspas.svg',
+      descripcionProducto: 'Aspas para ventilador Mazda B20000, B22000',
+      precio: '$ 1,842',
+      oferta: '$ 1,842',
+      ranking: '4.9',
+    },
+    {
+      sku: 'DCATO19939',
+      srcImg: 'assets/imagenes/pagina-principal/ofertas/escape.svg',
+      descripcionProducto: 'Escape resonador mofle',
+      precio: '$ 1,842',
+      oferta: '$ 1,842',
+      ranking: '4.9',
+    },
+    {
+      sku: 'DCATO19939',
+      srcImg: 'assets/imagenes/pagina-principal/ofertas/carter.svg',
+      descripcionProducto: 'Carter aceite Mazda 6 2.5, 20009 a 2010 aluminio',
+      precio: '$ 1,842',
+      oferta: '$ 1,842',
+      ranking: '4.9',
+    },
+    {
+      sku: 'DCATO19939',
+      srcImg: 'assets/imagenes/pagina-principal/ofertas/bujias.svg',
+      descripcionProducto: 'Kit 5 Bujias Ngk Platino Vw Jetta Mk6 Bora Beetle Motor 2.5l',
+      precio: '$ 1,842',
+      oferta: '$ 1,842',
+      ranking: '4.9',
+    },
+  ];
 
+  constructor() { }
 }
---
+++ src/app/modules/pagina-principal/models/oferta.ts
@@ -0,0 +1,8 @@
+export interface Oferta {
+    sku: string;
+    srcImg: string;
+    descripcionProducto: string;
+    precio: string;
+    oferta: string;
+    ranking?: string;
+}
