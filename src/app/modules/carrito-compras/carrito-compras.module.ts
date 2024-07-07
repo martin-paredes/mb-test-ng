ed441371e9a7ae56267a71346657b221f0afb2b1
Martín Paredes <martin.paredes@lucasianmexico.com>
2024-07-07 12:58:15 -0600

    Estructura inicial componente Carrito Compras

--- src/app/modules/carrito-compras/carrito-compras.module.ts
+++ src/app/modules/carrito-compras/carrito-compras.module.ts
@@ -5,6 +5,9 @@ import { CarritoComprasRoutingModule } from './carrito-compras-routing.module';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { CarritoComprasComponent } from './components/carrito-compras/carrito-compras.component';
+import { ListadoProductosComponent } from './components/listado-productos/listado-productos.component';
+import { ResumenComprasComponent } from './components/resumen-compras/resumen-compras.component';
+import { CompradosHabitualmenteComponent } from './components/comprados-habitualmente/comprados-habitualmente.component';
 
 
 @NgModule({
@@ -14,6 +17,9 @@ import { CarritoComprasComponent } from './components/carrito-compras/carrito-co
   imports: [
     CommonModule,
     CarritoComprasRoutingModule,
+    ResumenComprasComponent,
+    ListadoProductosComponent,
+    CompradosHabitualmenteComponent,
     HeaderComponent,
     FooterComponent,
   ]
