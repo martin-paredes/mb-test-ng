4eecbf98009ea97888038d241bda74a792f57573
Martín Paredes <martin.paredes@lucasianmexico.com>
2024-07-07 12:44:04 -0600

    Inicialización componente Carrito Comprar ( Header y Footer)

--- src/app/modules/carrito-compras/carrito-compras.module.ts
+++ src/app/modules/carrito-compras/carrito-compras.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CarritoComprasRoutingModule } from './carrito-compras-routing.module';
+import { HeaderComponent } from '../../components/header/header.component';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { CarritoComprasComponent } from './components/carrito-compras/carrito-compras.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarritoComprasComponent,
+  ],
   imports: [
     CommonModule,
-    CarritoComprasRoutingModule
+    CarritoComprasRoutingModule,
+    HeaderComponent,
+    FooterComponent,
   ]
 })
 export class CarritoComprasModule { }
--- src/app/modules/carrito-compras/components/carrito-compras/carrito-compras.component.ts
+++ src/app/modules/carrito-compras/components/carrito-compras/carrito-compras.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrito-compras',
-  standalone: true,
-  imports: [],
   templateUrl: './carrito-compras.component.html',
   styleUrl: './carrito-compras.component.sass'
 })
