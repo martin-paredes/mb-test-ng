843bd4fd79a9ea67c49b9c4d726f94b2f7a693f4
Martín Paredes <martin.paredes@lucasianmexico.com>
2024-07-13 15:25:29 -0600

    Finalización maquetación componente de Carrito Compra
    Se realiza e integra componente Resumen de Compra, se deja condiciones para usarse en componente de Terminar Compra

--- src/app/modules/carrito-compras/components/resumen-compras/resumen-compras.component.ts
+++ src/app/modules/carrito-compras/components/resumen-compras/resumen-compras.component.ts
@@ -1,12 +1,15 @@
+import { NgClass, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resumen-compras',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgStyle],
   templateUrl: './resumen-compras.component.html',
   styleUrl: './resumen-compras.component.sass'
 })
 export class ResumenComprasComponent {
+
+  public tipoResumenCompras: 'carritoCompras' | 'terminarCompra' = 'carritoCompras';
 
 }
